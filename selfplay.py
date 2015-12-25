4bd9f693d870d7097a5f0c7c2928827a3ca1cf1c
P. S. Clarke <yahtzee@pscl4rke.net>
2015-12-25 22:42:41 +0000

    Feature: Some hardcoded averages to beat.

--- selfplay.py
+++ selfplay.py
@@ -83,12 +83,44 @@ class CheckBeforeRollPlayer(BasePlayer):
         return game.UseCommand(self.best_scoring_row(card, hand).id)
 
 
+class BeatingLastAverages(BasePlayer):
+
+    def decide_next_move(self, card, hand):
+        averages = {
+            "total1": 2,
+            "total2": 4,
+            "total3": 6,
+            "total4": 7,
+            "total5": 8,
+            "total6": 9,
+            "atleast3": 16,
+            "atleast4": 15,
+            "fullhouse": 23,
+            "short": 27,
+            "long": 12,
+            "yahtzee": 19,
+            "chance": 16,
+        }
+        for row_id in ("fullhouse", "long", "short"):
+            if card.row_by_id(row_id).can_be_used():
+                if card.row_by_id(row_id).scorer.score(hand) > 0:
+                    return game.UseCommand(row_id)
+        if hand.has_rolls_left():
+            return game.RerollNotCommand(hand.most_popular_faces()[0], hand)
+        possibilities = [
+            (row.scorer.score(hand) - averages[row.id], row)
+            for row in card.available_rows() ]
+        possibilities.sort()
+        return game.UseCommand(possibilities[-1][1].id)
+
+
 def main():
     players_to_try = [
         RandomPlayer,
         GrabMaxPlayer,
         RollThenGrabPlayer,
         CheckBeforeRollPlayer,
+        BeatingLastAverages,
     ]
     for player_class in players_to_try:
         runner = Runner(player_class)
